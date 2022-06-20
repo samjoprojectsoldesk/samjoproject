a35bed0c7eae5e0a0b7e76ff43b7b108c93ca8a1
pyo seongwoo <pyosw12@gmail.com>
2022-06-20 15:53:16 +0900

    장바구니 CRUD

--- samjo/src/main/java/kr/co/samjo/cart/cartCont.java
+++ samjo/src/main/java/kr/co/samjo/cart/cartCont.java
@@ -1,7 +1,11 @@
 package kr.co.samjo.cart;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,13 +16,58 @@ public class cartCont {
 		dao = new cartDAO();
 		System.out.println("-----cartCont객체 생성됨");
 	}
+	
+	@RequestMapping(value="cart/add.do", method=RequestMethod.POST)
+	public ModelAndView cartAdd(@ModelAttribute cartDTO dto) {
+		ModelAndView mav=new ModelAndView();
+		mav.setViewName("cart/msgView");
+		
+		int cnt=dao.create(dto);
+		if(cnt==0) {
+			String msg="<p>장바구니 등록 실패</p>";
+			String img="<img src='../images/fail.png'>";
+			String link1="<input type='button' value='다시시도' onclick='javascript:history.back()'>";
+			String link2="<input type='button' value='장바구니목록' onclick='location.href=\"list.do\"'>";
+			mav.addObject("msg", msg);
+			mav.addObject("img", img);
+			mav.addObject("link1", link1);
+			mav.addObject("link2", link2);
+		}else {
+			String msg="<p>장바구니 등록 성공</p>";
+			String img="<img src='../images/sound.png'>";
+			String link2="<input type='button' value='장바구니목록' onclick='location.href=\"list.do\"'>";
+			mav.addObject("msg", msg);
+			mav.addObject("img", img);
+			mav.addObject("link2", link2);
+		}
+		return mav;
+	}
 
 	@RequestMapping("/cart/list.do")
-	public ModelAndView list() {
+	public ModelAndView list(String user_id) {
 		ModelAndView mav=new ModelAndView();
 		mav.setViewName("cart/list");
-		mav.addObject("list", dao.list());
+		mav.addObject("list", dao.list(user_id));
+		mav.addObject("user_id", user_id);
 		return mav;
 	}//list end
 	
+	@RequestMapping(value = "cart/delete.do", method = RequestMethod.GET)
+	public ModelAndView deleteForm(int c_no) {
+		ModelAndView mav=new ModelAndView();
+		mav.setViewName("cart/deleteForm");
+		mav.addObject("c_no", c_no);
+		return mav;
+	}//deleteForm end
+
+	@RequestMapping(value = "cart/delete.do", method = RequestMethod.POST)
+	public ModelAndView deleteProc(int c_no) {
+		ModelAndView mav=new ModelAndView();
+		mav.setViewName("cart/msgView");
+		
+		int cnt = dao.delete(c_no);
+		if(cnt == 0) {}else {}
+		
+		return mav;
+	}//deleteProc end
 }
--- samjo/src/main/java/kr/co/samjo/cart/cartDAO.java
+++ samjo/src/main/java/kr/co/samjo/cart/cartDAO.java
@@ -15,17 +15,20 @@ public class cartDAO {
 	private ResultSet rs=null;
 	private StringBuilder sql=null;
 
-	public ArrayList<cartDTO> list(){
+	public ArrayList<cartDTO> list(String user_id){
 		ArrayList<cartDTO> list=null;
         try {
             con=dbopen.getConnection();
             
             sql=new StringBuilder();
             sql.append(" SELECT c_no, user_id, s_code, cnt, p_cnt, sdate, fdate ");
-            sql.append(" FROM cart ");
+            sql.append(" FROM tb_cart ");
+            sql.append(" WHERE user_id=? ");
             sql.append(" ORDER BY c_no DESC ");
             
             pstmt=con.prepareStatement(sql.toString());
+            pstmt.setString(1, user_id);
+            
             rs=pstmt.executeQuery();
             if(rs.next()) {
                 list=new ArrayList<cartDTO>();
@@ -49,5 +52,53 @@ public class cartDAO {
         }//end		
 		return list;
 	}//list() end
-	
+
+	public int create(cartDTO dto) {
+		int cnt=0;
+		try {
+			con=dbopen.getConnection(); //DB연결
+			
+			sql=new StringBuilder();
+
+			sql.append(" INSERT INTO tb_cart(c_no, user_id, s_code, cnt, p_cnt, sdate, fdate) ");
+			sql.append(" VALUES( cart_seq.nextval, ?, ?, ?, ?, ?, ?) ");
+			
+			pstmt=con.prepareStatement(sql.toString());
+			pstmt.setString(1, dto.getUser_id());
+			pstmt.setString(2, dto.getS_code());
+			pstmt.setInt(3, dto.getCnt());
+			pstmt.setInt(4, dto.getP_cnt());
+			pstmt.setString(5, dto.getSdate());
+			pstmt.setString(6, dto.getFdate());
+			
+			cnt=pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			System.out.println("장바구니등록실패" + e);
+		}finally {
+			DBClose.close(con,pstmt);
+		}
+		return cnt;
+	}
+
+	public int delete(int c_no) {
+		int cnt=0;
+		try {			
+			con=dbopen.getConnection(); //DB연결
+			sql=new StringBuilder();
+			sql.append(" DELETE FROM tb_cart ");
+			sql.append(" WHERE c_no=? ");
+			
+			pstmt=con.prepareStatement(sql.toString());
+			pstmt.setInt(1, c_no);
+			cnt=pstmt.executeUpdate();
+						
+		} catch (Exception e) {
+            System.out.println("장바구니 목록 삭제 실패:"+e);
+        } finally {
+            DBClose.close(con, pstmt);
+        }//end
+		
+		return cnt;
+	}	
 }
