8cc046bf0fd9cb9dd4104fb5927d2c59ac03f8ca
jisoo <wl6717@naver.com>
2022-06-22 15:04:00 +0900

    내용정리

--- samjo/src/main/java/net/utility/DBClose.java
+++ samjo/src/main/java/net/utility/DBClose.java
@@ -5,7 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class DBClose {
-<<<<<<< HEAD
 	//DB연결 자원 반납
 	
 	public static void close(Connection con) {
@@ -13,20 +12,11 @@ public class DBClose {
 			if(con!=null) {con.close();}
 		}catch(Exception e) {}
 	}//end
-=======
 
-	//DB연결 자원 반납
-	public static void close(Connection con) {
-		try {
-			if(con!=null) {con.close();}
-		} catch (Exception e) {}
-	}
->>>>>>> 0e885f2360e516d5663ab3e7a3d3b1eefa40f3a5
 	
 	public static void close(Connection con, PreparedStatement pstmt) {
 		try {
 			if(pstmt!=null) {pstmt.close();}
-<<<<<<< HEAD
 		}catch(Exception e) {}
 		
 		try {
@@ -49,23 +39,3 @@ public class DBClose {
 	}//end
 	
 }//class end
-=======
-		} catch (Exception e) {}
-		try {
-			if(con!=null) {con.close();}
-		} catch (Exception e) {}
-	}
-	
-	public static void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
-		try{
-			if(rs!=null) {rs.close();}
-		}catch (Exception e){}
-		try {
-			if(pstmt!=null) {pstmt.close();}
-		} catch (Exception e) {}
-		try {
-			if(con!=null) {con.close();}
-		} catch (Exception e) {}
-	}
-}
->>>>>>> 0e885f2360e516d5663ab3e7a3d3b1eefa40f3a5
--- samjo/src/main/java/net/utility/DBOpen.java
+++ samjo/src/main/java/net/utility/DBOpen.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBOpen {
-<<<<<<< HEAD
 	
 	//오라클 DB 연결 메소드
 	public Connection getConnection() {
@@ -26,23 +25,4 @@ public class DBOpen {
 		
 		return con;
 	}//getConnection() end
-}//cladd end
-=======
-	//오라클 DB 연결 메소드
-	public Connection getConnection() {
-		Connection con=null;
-		try {
-			String url="jdbc:oracle:thin:@localhost:1521:xe";
-			String user="system";
-			String password="1234";
-			String driver="oracle.jdbc.driver.OracleDriver"; //ojdbc8.jar
-			Class.forName(driver);
-			con = DriverManager.getConnection(url,user,password);
-		} catch (Exception e) {
-			System.out.println("오라클DB연결실패:" + e);
-		}
-		
-		return con;
-	}
-}
->>>>>>> 0e885f2360e516d5663ab3e7a3d3b1eefa40f3a5
+}//class end
